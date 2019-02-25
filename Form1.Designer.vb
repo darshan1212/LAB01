b76b43136f503740b469997d1bfb5099895d79ce
darshan1212 <47987977+darshan1212@users.noreply.github.com>
2019-02-25 13:04:46 -0500

    InitialCommit

---
+++ Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.richTxtBox = New System.Windows.Forms.RichTextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblUnits
+        '
+        Me.lblUnits.AutoSize = True
+        Me.lblUnits.Location = New System.Drawing.Point(78, 16)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(34, 13)
+        Me.lblUnits.TabIndex = 0
+        Me.lblUnits.Text = "Units:"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(118, 13)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(43, 20)
+        Me.txtInput.TabIndex = 1
+        Me.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblDay
+        '
+        Me.lblDay.AutoSize = True
+        Me.lblDay.Location = New System.Drawing.Point(188, 16)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(38, 13)
+        Me.lblDay.TabIndex = 2
+        Me.lblDay.Text = "Day 1:"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.lblAverage)
+        Me.Panel1.Location = New System.Drawing.Point(81, 174)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(145, 43)
+        Me.Panel1.TabIndex = 6
+        '
+        'lblAverage
+        '
+        Me.lblAverage.AutoSize = True
+        Me.lblAverage.Location = New System.Drawing.Point(3, 14)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(0, 13)
+        Me.lblAverage.TabIndex = 0
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(12, 223)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 7
+        Me.btnEnter.Text = "E&nter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(118, 223)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 8
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(223, 223)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'richTxtBox
+        '
+        Me.richTxtBox.BackColor = System.Drawing.SystemColors.Window
+        Me.richTxtBox.Location = New System.Drawing.Point(81, 39)
+        Me.richTxtBox.Name = "richTxtBox"
+        Me.richTxtBox.ReadOnly = True
+        Me.richTxtBox.Size = New System.Drawing.Size(145, 129)
+        Me.richTxtBox.TabIndex = 10
+        Me.richTxtBox.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(310, 258)
+        Me.Controls.Add(Me.richTxtBox)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblUnits)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.Text = "Average Units Shipped"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblDay As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblAverage As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents richTxtBox As RichTextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+    Dim listUnits As New List(Of String)
+    Dim averageUnits As Integer = 0
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        Const DaysInWeek As Short = 7
+
+        Dim enteredNumber As Integer
+
+        If Integer.TryParse(txtInput.Text, enteredNumber) Then
+            ' text is convertible to Double, and value contains the Double value now
+            listUnits.Add(txtInput.Text)
+            richTxtBox.Lines = listUnits.ToArray()
+            averageUnits = averageUnits + enteredNumber
+            lblDay.Text = "Day " & listUnits.Count & " :"
+        Else
+            ' Cannot convert text to Double
+            MessageBox.Show("Enter valid input. Input must be an integer")
+            ' reset code goes over here
+            txtInput.SelectionStart = 0
+            txtInput.DeselectAll()
+            txtInput.SelectAll()
+
+        End If
+
+        If (listUnits.Count = DaysInWeek) Then
+            averageUnits = averageUnits / DaysInWeek
+            lblAverage.Text = "Average Units Shipped:" + averageUnits.ToString
+            listUnits.Clear()
+            averageUnits = 0
+            txtInput.Clear()
+            btnEnter.Enabled = False
+            txtInput.Enabled = False
+        End If
+
+
+    End Sub
+
+    Private Sub richTxtBox_TextChanged(sender As Object, e As EventArgs) Handles richTxtBox.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        listUnits.Clear()
+        averageUnits = 0
+        txtInput.Clear()
+        richTxtBox.Clear()
+        lblAverage.Text = ""
+        lblDay.Text = "Day 1:"
+        btnEnter.Enabled = True
+        txtInput.Enabled = True
+    End Sub
+
+    Private Sub txtInput_TextChanged(sender As Object, e As EventArgs) Handles txtInput.TextChanged
+
+    End Sub
+End Class
